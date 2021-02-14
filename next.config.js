cc11acced7fb20472501ebc20879bae7f8bd8818
dperolio <dustin.perolio@outlook.com>
2021-02-13 20:56:21 -0500

    Start setting up Dokz for documentation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const { withDokz } = require('dokz/dist/plugin');
+
+const nextConfiguration = {
   // Target must be serverless
   target: 'serverless'
 };
+
+const redirects = {
+  async redirects () {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/start/getting-started',
+        permanent: true
+      }
+    ];
+  }
+};
+
+module.exports = withPlugins(
+  [
+    [ withDokz, { pageExtensions: [ 'js', 'jsx', 'md', 'mdx', 'ts', 'tsx' ] } ],
+    [ redirects ]
+  ],
+  nextConfiguration
+);
