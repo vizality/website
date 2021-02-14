9ab12c3c964701abcbd3e73055193c87a64d2a1a
dperolio <dustin.perolio@outlook.com>
2021-02-13 21:30:51 -0500

    Set up jsconfig better, export components

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from './Button';
+export { default as Canvas } from './Canvas';
+export { default as DelayedRender } from './DelayedRender';
+export { default as Icon } from './Icon';
