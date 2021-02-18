269beebc6d8530ae0af6c387f4356e0b824e454a
dperolio <dustin.perolio@outlook.com>
2021-02-18 11:59:57 -0500

    Testing API routes

---
+++ pages/api/test.js
@@ -0,0 +1,30 @@
+import Cors from 'cors';
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: [ 'GET', 'HEAD' ]
+});
+
+/*
+ * Helper method to wait for a middleware to execute before continuing
+ * And to throw an error when an error happens in a middleware
+ */
+function runMiddleware (req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler (req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' });
+}
+
