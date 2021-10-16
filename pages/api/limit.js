de2edfe35b52e7504b7673c22af9e96ee3aaafe4
dperolio <dustin.perolio@outlook.com>
2021-10-16 19:57:39 -0400

    test plugin api subroutes, add a relative time to api limit return

--- pages/api/limit.js
+++ pages/api/limit.js
@@ -30,10 +30,35 @@ function runMiddleware (req, res, fn) {
   });
 }
 
+function getRelativeTime (current, previous) {
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+  const elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    return `${Math.round(elapsed / 1000)} seconds`;
+  } else if (elapsed < msPerHour) {
+    return `${Math.round(elapsed / msPerMinute)} minutes`;
+  } else if (elapsed < msPerDay) {
+    return `${Math.round(elapsed / msPerHour)} hours`;
+  } else if (elapsed < msPerMonth) {
+    return `approximately ${Math.round(elapsed / msPerDay)} days`;
+  } else if (elapsed < msPerYear) {
+    return `approximately ${Math.round(elapsed / msPerMonth)} months`;
+  }
+  return `approximately ${Math.round(elapsed / msPerYear)} years`;
+}
+
 export default async function handler (req, res) {
-  // Run the middleware
+  /**
+   * Run the middleware.
+   */
   await runMiddleware(req, res, cors);
   const limit = await github.rateLimit.get();
-  // Rest of the API logic
-  res.json({ message: limit?.data?.resources?.core });
+  const limitData = { ...limit?.data?.resources?.core };
+  limitData.relative_reset = getRelativeTime(new Date(), new Date(limit?.data?.resources?.core?.reset))
+  res.json({ message: limitData });
 }
---
+++ pages/api/plugin/[id].js
@@ -0,0 +1,32 @@
+import Cors from 'cors';
+
+/**
+ * Initialize the cors middleware.
+ */
+const cors = Cors({
+  methods: [ 'GET', 'HEAD' ]
+});
+
+/**
+ * Helper method to wait for a middleware to execute before continuing.
+ * And to throw an error when an error happens in a middleware.
+ */
+function runMiddleware (req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler (req, res) {
+  const { id } = req.query;
+  /**
+   * Run the middleware.
+   */
+  await runMiddleware(req, res, cors);
+  res.json({ message: `test plugin ${id}` });
+}
