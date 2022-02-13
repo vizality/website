526f555648a0a4bb5e029e434390739c7396ba97
dperolio <dustin@dperolio.com>
2022-02-13 17:21:34 -0500

    remove bot client and detritus completely

--- pages/api/discord/banners/[userId].js
+++ pages/api/discord/banners/[userId].js
@@ -1,25 +1,5 @@
-import { Constants as SocketConstants } from 'detritus-client-socket';
-import { InteractionCommandClient } from 'detritus-client';
 import Cors from 'cors';
 
-/**
- * Instantiate the interaction command client with various options.
- */
-const interactionClient = new InteractionCommandClient(process.env.BOT_TOKEN, {
-  useClusterClient: false,
-  gateway: {
-    intents: [
-      SocketConstants.GatewayIntents.GUILDS,
-      SocketConstants.GatewayIntents.GUILD_MEMBERS,
-      SocketConstants.GatewayIntents.GUILD_MESSAGES,
-      SocketConstants.GatewayIntents.GUILD_PRESENCES,
-      SocketConstants.GatewayIntents.GUILD_MESSAGE_REACTIONS,
-      SocketConstants.GatewayIntents.DIRECT_MESSAGES,
-      SocketConstants.GatewayIntents.DIRECT_MESSAGE_REACTIONS,
-      SocketConstants.GatewayIntents.GUILD_VOICE_STATES
-    ]
-  }
-});
 
 /**
  * Initialize the cors middleware.
