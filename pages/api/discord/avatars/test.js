3ea4e3e515edacc6c76b7eb4b48140d108c4fc5e
dperolio <dustin@dperolio.com>
2022-02-13 12:52:50 -0500

    add test api route to try to figure out 502 error

---
+++ pages/api/discord/avatars/test.js
@@ -0,0 +1,32 @@
+import Cors from 'cors';
+
+/**
+ * Initialize the cors middleware.
+ */
+const cors = Cors({
+  methods: [ 'GET', 'HEAD' ]
+});
+
+/**
+ * Helper method to wait for a middleware to execute before continuing.
+ * And to throw an error when an error happens in a middleware.
+ */
+function runMiddleware (req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler (req, res) {
+  /**
+   * Run the middleware.
+   */
+  await runMiddleware(req, res, cors);
+
+  res.status(200).json({ message: 'yes' });
+}
